9cf15e5018cc0b1520ccaa2ac1bc5d93c7381cdf
LMDominguez <lmdominguez@protonmail.com>
2023-04-24 01:48:01 +0000

    Updates with Nathanael

--- routes.js
+++ routes.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
+function authorize(level) {
+  return (req, res, next) => {
+    const user = {
+      level: 5
+    }
+    if (user.level >= level) next()
+      else res.send('Access denied')
+  }
+}
+
 // Index route
-router.get('/', (req, res) => {
+router.get('/', authorize(3), (req, res) => {
   res.render('index')
 })
 
 // About route
-router.get('/about', (req, res) => {
+router.get('/about', authorize(1), (req, res, next) => {
   res.render('about')
+  console.log(1)
+  next()
 })
 
 // Users route
-router.get('/users', (req, res) => {
+router.get('/users', authorize(1), (req, res) => {
   res.render('users')
 })
 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/static', express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello, World!' });
@@ -14,6 +17,24 @@ const routes = require('./routes')
 // Use routes
 app.use('/', routes)
 
-app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000');
-});
+app.use((req, res, next) => {
+  console.log(req.url)
+  next()
+})
+
+// 404 error handleing
+
+
+
+// app.listen(3000, () => {
+//   console.log('Server listening on http://localhost:3000');
+// });
+
+// server http
+const http = require('http')
+const PORT = process.env.PORT || 6000 //first checks for environment variable delcaring port number, if not defualt 5000
+const server = http.createServer(app)
+server.listen(PORT, () => {
+  console.log(app.get('views'))
+})
+
